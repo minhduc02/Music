1ef46868e2119eaf0fe472aeb2b8c44186154d69
Minh Đức <vmd.vuminhduc2002@gmail.com>
2024-08-15 14:58:41 +0700

    begin

---
+++ app/src/main/java/com/minhduc02/music/MainActivity.java
@@ -0,0 +1,14 @@
+package com.minhduc02.music;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
